e97b2d885c1729484c1e6083c7bb89025b8f8d54
wondieki <wkerubo28@gmail.com>
2024-11-04 09:16:16 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+const donors=["Guadalupe", "Ollie", "Aki"];
+let thankYouMessages=[];
+function writeCards(donors){
+    for (let i = 0; i < donors.length; i++) {
+    thankYouMessages.push(`Thank you, ${donors[i]}, for the wonderful surprise gift!`) 
+    }
+    return thankYouMessages;
+}
+function countDown (){
+    for (let number=10; number>-1; number-=1)
+    {console.log(number);}
+}
